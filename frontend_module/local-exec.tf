1683ddae1f4e8441671ac2716ab614d8641e9a0c
ahmadrazalab <razahmadrsofficial@gmail.com>
2024-09-18 15:51:57 +0530

    backend full module

--- frontend_module/local-exec.tf
+++
@@ -1,6 +0,0 @@
-resource "null_resource" "upload_build_s3_files" {
-  provisioner "local-exec" {
-    command = "aws s3 cp ./dash s3://${var.static_site_bucket_name} --recursive"
-  }
-  depends_on = [aws_s3_bucket.prod_static_site]
-}
